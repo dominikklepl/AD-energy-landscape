1ac3a900d4e3d40c02f40e5fea1713b53df6f997
Dominik Klepl <dominikklepl@macbook-air.home>
2020-06-22 10:09:47 +0200

    basin size plot

--- scripts/ELA.R
+++ scripts/ELA.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(tidygraph)
+library(ggraph)
 
 get_energy = function(parameter_list) {
   h = parameter_list$h
@@ -57,4 +59,66 @@ find_minima = function(adj, states_energy){
   minima = data.frame(state = minima, energy = states_energy$energy[states_energy$state %in% minima])
   
   return(minima)
+}
+
+basin_size_estimator = function(adjancency, states_energy){
+  basin_membership = rep(NA, ncol(adjancency))
+  positions = data.frame(from=NA,to =NA)
+  
+  iter = 1e8 #random large number
+  
+  for (i in 1:ncol(adjancency)){
+    start = colnames(adjancency)[i]
+    for (j in 1:iter){
+      start_E = states_energy$energy[states_energy$state==start]
+      neighbours = rownames(adjancency)[adjancency[,start]==1]
+      neighbours_E =  states_energy$energy[states_energy$state %in% neighbours]
+      
+      states = c(start,neighbours)
+      E = c(start_E,neighbours_E)
+      new = states[order(E)[1]]
+      positions[nrow(positions)+1,] = c(start,new)
+      
+      if (start==new){break}
+      else {start=new}
+    }
+    basin_membership[i] = start
+  }
+  
+  basin_membership = data.frame(state = colnames(adjancency),
+                                basin = basin_membership)
+  result = list(membership = basin_membership,positions = positions)
+  return(result)
+}
+
+BinToDec <- function(x) sum(2^(which(rev(unlist(strsplit(as.character(x), "")) == 1))-1))
+
+plot_basin = function(basin_df) {
+  pos_dec = basin_df$positions[-1,] %>% distinct()
+  mem_dec = basin_df$membership
+  
+  pos_dec$from = sapply(pos_dec$from,BinToDec)
+  pos_dec$to = sapply(pos_dec$to,BinToDec)
+  
+  mem_dec$state = sapply(mem_dec$state,BinToDec)
+  mem_dec$basin = sapply(mem_dec$basin,BinToDec)
+  colnames(mem_dec)[1] = "from"
+  
+  basin_network = pos_dec %>%
+    full_join(mem_dec, by = "from")
+  basin_network$basin = as.factor(basin_network$basin)
+  
+  tidy = as_tbl_graph(basin_network) 
+  
+  plot = tidy %>%
+    activate(nodes) %>%
+    mutate(color = .E()$basin) %>%
+    activate(edges) %>%
+    ggraph(layout="stress")+
+    geom_edge_link()+
+    geom_node_point(aes(colour=color),size=1)+
+    scale_color_tableau()+
+    guides(color=guide_legend(title = "Local minimum"))+
+    theme_few()
+  return(plot)
 }
